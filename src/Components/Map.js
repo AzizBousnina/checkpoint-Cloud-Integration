7de8c625f78544496059e199d5efa72139ec6cfe
AzizBousnina <azizbousnina2000@gmail.com>
2023-04-08 14:42:39 +0200

    first commit

---
+++ src/Components/Map.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+
+        <div>
+            <h1>GoMyCode HackerSpace El Manzah in Google Maps</h1>
+        <iframe title='maps' width="800" height="550" src="https://maps.google.com/maps?q=gomycode%20menzah&t=&z=13&ie=UTF8&iwloc=&output=embed" >
+        </iframe>
+        
+        </div>
+
+
+  )
+}
+
+export default Map
